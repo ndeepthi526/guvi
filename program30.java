2256c2781ccbb2ad99a9857c2585eae8e925428a
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-11 12:21:37 +0530

    Create program30.java

---
+++ program30.java
@@ -0,0 +1,39 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int h1,h2,m1,m2,d,h3,h4;
+		Scanner s=new Scanner(System.in);
+		h1=s.nextInt();
+		m1=s.nextInt();
+		h2 =s.nextInt();
+		m2=s.nextInt();
+		h3=h1*60+m1;
+		h4=h2*60+m1;
+		d=h3-h4;
+		if(d>60)
+		{
+			h3=d/60;
+			h4=d%60;
+			System.out.println(h3+ " " +h4);
+		}
+		else
+		{
+			System.out.println(d);
+		}
+	}
+}
+		
+	
+		
+		
+			
+		
+	
