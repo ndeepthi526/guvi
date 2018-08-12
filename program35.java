adabb1e3c60c67544a9292447fd7d8d69a9e1eeb
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-12 19:53:56 +0530

    Create program35.java

---
+++ program35.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+String str;
+
+int count=0;
+
+Scanner scan= new Scanner(System.in);
+
+str=scan.nextLine();
+
+for(int i=0;i<str.length();i++)
+
+{
+ 
+   if(str.charAt(i)>='0'&&str.charAt(i)<='9')
+
+    {
+
+        count++;
+  
+  }
+
+}
+
+System.out.println(count);
+
+		
+	}
+
+}
+	
