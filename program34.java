f3e44fc4efcf9e3f2a540e935863bfa2aad3dd3d
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-12 19:28:46 +0530

    Create program34.java

---
+++ program34.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int count=0;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i )== '.' )
+			{
+				count=count+1;
+			}
+		}
+			System.out.println(count);
+		
+	}
+}
+	
+
+		
+	
+	
