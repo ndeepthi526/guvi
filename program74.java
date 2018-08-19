9bb8afff1eed0ae0e114d32124228a59dab84be3
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-19 20:11:36 +0530

    Create program74.java

---
+++ program74.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.ArrayList;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float n;
+		int a;
+		Scanner s=new Scanner(System.in);
+		n=s.nextFloat();
+		a=Math.round(n);
+		System.out.println(a);
+	
+		
+	}
+}
