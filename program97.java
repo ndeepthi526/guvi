78f5d8433aafbf3fc633572550110167368ccd88
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-27 16:18:22 +0530

    Create program97.java

---
+++ program97.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     int n,rev=0;
+	     Scanner s=new Scanner(System.in);
+	     n=s.nextInt();
+	     while(n!=0)
+	     {
+	     	rev=rev*10;
+	     	rev=rev+n%10;
+	     	n=n/10;
+	     }
+	     System.out.println(rev);
+	}
+}
