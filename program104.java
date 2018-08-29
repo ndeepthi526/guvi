4f88ac9d86a6eefb84292cebea792912916b8d1f
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-29 10:44:21 +0530

    Create program104.java

---
+++ program104.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		double n,k,a;
+		Scanner s=new Scanner(System.in);
+		n=s.nextDouble();
+		k=s.nextDouble();
+		a=Math.pow(n,k);
+		System.out.println(a);
+	}
+}
