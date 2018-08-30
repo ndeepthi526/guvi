269c8ab91168620cb68e0dea2f83e0edc9397029
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-08-30 10:50:08 +0530

    Create p5.java

---
+++ p5.java
@@ -0,0 +1,129 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int b=0;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		char a[]=str.toCharArray();
+		int l=str.length();
+		for(int i=0;i<l;i++)
+		{
+			switch(a[i])
+			{
+				case 'I':
+					b=1;
+					System.out.println(b);
+					break;
+				case II:
+					b=2;
+					System.out.println(b);
+					break;
+				case III:
+					b=3;
+					System.out.println(b);
+					break;
+				case IV:
+					b=4;
+					System.out.println(b);
+					break;
+				case 'V':
+					b=5;
+					System.out.println(b);
+					break;
+				case VI:
+					b=6;
+					System.out.println(b);
+					break;
+			   case VII:
+					b=7;
+					System.out.println(b);
+					break;
+				case VIII:
+					b=8;
+					System.out.println(b);
+					break;
+				case IX:
+					b=9;
+					System.out.println(b);
+					break;
+				case 'X':
+					b=10;
+					System.out.println(b);
+					break;
+				case XI:
+					b=11;
+					System.out.println(b);
+					break;
+				case XII:
+					b=12;
+					System.out.println(b);
+					break;
+				case XIII:
+					b=13;
+					System.out.println(b);
+					break;
+				case XIV:
+					b=14;
+					System.out.println(b);
+					break;
+				case XV:
+					b=15;
+					System.out.println(b);
+					break;
+				case XVI:
+					b=16;
+					System.out.println(b);
+					break;
+				case XVII:
+					b=17;
+					System.out.println(b);
+					break;
+				case XVIII:
+					b=18;
+					System.out.println(b);
+					break;
+				case XIX:
+					b=19;
+					System.out.println(b);
+					break;
+				case XX:
+					b=1;
+					System.out.println(b);
+					break;
+				default:
+				break;
+			}
+			
+		}
+	}
+}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+					
+					
+				
+					
+					
+					
+			
+		
+	
