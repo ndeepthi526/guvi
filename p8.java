5e852187ed9dc5abfa7140f4617cff134572af0f
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-04 11:57:31 +0530

    Update p8.java

--- p8.java
+++ p8.java
@@ -16,12 +16,15 @@ class Ideone
         String result = "";
         char firstChar = str.charAt(0);
         result = result + Character.toUpperCase(firstChar);
-        for (int i = 1; i < str.length(); i++) {
+        for (int i = 1; i < str.length(); i++) 
+	{
             char currentChar = str.charAt(i);
             char previousChar = str.charAt(i - 1);
-            if (previousChar == ' ') {
+            if (previousChar == ' ')
+	    {
                 result = result + Character.toUpperCase(currentChar);
-            } else {
+            } else
+	    {
                 result = result + currentChar;
             }
         }
