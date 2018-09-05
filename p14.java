b8335b12c7c71b7e71b4add67b03086e2b40a176
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-05 14:47:02 +0530

    Create p14.java

---
+++ p14.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str1,str2,str3;
+		Scanner s=new Scanner(System.in);
+		str1=s.next();
+		str2=str1.replaceAll("[aeiouAEIOU]","");
+		str3=new StringBuilder(str2).reverse().toString();
+		System.out.println(str3);
+	}
+}
+		
+	
