f5121d543aa747fe61b723b41bb4f3895671a58a
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-06 12:34:04 +0530

    Create p25.java

---
+++ p25.java
@@ -0,0 +1,45 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has toro be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i,j;
+		String str;
+		String temp;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		str=s.nextLine();
+		String a[]=new String[n];
+		for( i=0;i<n;i++)
+		{
+			a[i]=s.nextLine();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=1;j<n;j++)
+			{
+				if(a[j-1].compareTo(a[j])>0)
+				{
+					temp=a[j-1];
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
+		
+				
+			
+		
+	
