9c851560871303f103eb60714517f8e5780fbc03
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-06 10:15:44 +0530

    Create p22.java

---
+++ p22.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int m,n,gcd=0;
+		Scanner s=new Scanner(System.in);
+		m=s.nextInt();
+		n=s.nextInt();
+		for(int i=1;i<=n && i<=m;i++)
+		{
+			if(n%i==0 && m%i==0)
+			{
+				gcd=i;
+			}
+		}
+		System.out.println(gcd);
+	}
+}
+		
+	
+	
