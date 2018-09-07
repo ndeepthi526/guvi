d584ca330d4f40f9a840a95c5330ffb02a649d66
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-07 12:54:18 +0530

    Create p27.java

---
+++ p27.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int i;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		char ch[]=str.toCharArray();
+		for(i=0;i<str.length();i++)
+		{
+			if(Character.isUpperCase(ch[i]))
+			{
+				ch[i]=Character.toLowerCase(ch[i]);
+			}
+			else
+			{
+				ch[i]=Character.toUpperCase(ch[i]);
+			}
+		}
+		for(i=0;i<str.length();i++)
+		{
+			System.out.println(ch[i]);
+		}
+	}
+}
+			
+			
+		
+				
+			
