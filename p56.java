a8e7f1bd6ae9981e01ddb7e3292c2abb9a7f1109
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-10 16:41:49 +0530

    Create p56.java

---
+++ p56.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		char k;
+		int i;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		k=s.next().charAt(0);
+		for(i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)==k)
+			{
+				System.out.println(i+1);
+				break;
+			}
+		}
+	}
+}
