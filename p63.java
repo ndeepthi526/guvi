b03c52c864d00003ac28c7e9df1f14d32108a7e4
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-11 12:01:25 +0530

    Create p63.java

---
+++ p63.java
@@ -0,0 +1,37 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i,j;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		int b[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(j=0;j<n;j++)
+		{
+			b[j]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(a[i]==b[j])
+				{
+					System.out.println(a[i]+" ");
+					break;
+				}
+			}
+		}
+	}
+}
