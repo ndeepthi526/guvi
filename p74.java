0f80457386363208643dc82bcf1868908c338f76
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-09-20 15:18:33 +0530

    Create p74.java

---
+++ p74.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,r=1,i;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		for(i=1;i<=b;i++)
+		{
+			
+			r=r*a;
+			
+			
+		}
+		System.out.println(r);
+	}
+}
