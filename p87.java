8af9eccdbf712a9c908de77bb1501039416d4a9e
ndeepthi526 <41731212+ndeepthi526@users.noreply.github.com>
2018-10-04 20:07:24 +0530

    Create p87.java

---
+++ p87.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,k,d=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+		d=n<<k;
+		System.out.println(d);
+		
+		
+	}
+}
